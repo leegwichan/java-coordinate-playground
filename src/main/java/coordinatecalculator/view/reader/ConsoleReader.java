638ae4e9a41a8ffd4789604713c3f8b95d5e7494
DESKTOP-E5VRH4Q\cnddk <cnddkscndgus@naver.com>
2023-08-09 13:49:15 +0900

    feat (Console) : 단순 입력/출력 기능 구현

---
+++ src/main/java/coordinatecalculator/view/reader/ConsoleReader.java
@@ -0,0 +1,13 @@
+package coordinatecalculator.view.reader;
+
+import java.util.Scanner;
+
+public class ConsoleReader implements Reader {
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    @Override
+    public String read() {
+        return scanner.nextLine();
+    }
+}
