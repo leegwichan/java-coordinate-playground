81cf992b90b8c1b099bafb8f6feda33a982c9dc1
leegwichan <cnddkscndgus@naver.com>
2023-08-09 23:04:53 +0900

    feat (OutputView) : 좌표 출력 기능 - 중간 구현
    - 좌표계 최대 최소 출력값 Validation 구현
    - 좌표계 정상 출력 테스트 코드 작성

--- src/main/java/coordinatecalculator/view/OutputView.java
+++ src/main/java/coordinatecalculator/view/OutputView.java
@@ -1,10 +1,19 @@
 package coordinatecalculator.view;
 
 import java.util.Objects;
+import coordinatecalculator.dto.PointDto;
+import coordinatecalculator.dto.PointsDto;
 import coordinatecalculator.view.printer.ConsolePrinter;
 import coordinatecalculator.view.printer.Printer;
 
-public class OutputView {
+public final class OutputView {
+
+    private static final int MAX_COORDINATE_RANGE = 24;
+    private static final int MIN_COORDINATE_RANGE = 1;
+
+    private static final String BLANK = "  ";
+    private static final String POINT = " •";
+    private static final String LINE_SEPARATOR = "\n";
 
     private final Printer printer;
 
@@ -18,5 +27,25 @@ public class OutputView {
 
     public static OutputView createConsoleOutputView() {
         return of(new ConsolePrinter());
+    }
+
+    public void printCoordinate(PointsDto points) {
+        validatePoints(points);
+    }
+
+    private void validatePoints(PointsDto points) {
+        for (PointDto point : points.getPoints()) {
+            validatePoint(point);
+        }
+    }
+
+    private void validatePoint(PointDto point) {
+        int xValue = point.getX();
+        int yValue = point.getY();
+
+        if (xValue < MIN_COORDINATE_RANGE || xValue > MAX_COORDINATE_RANGE
+                || yValue < MIN_COORDINATE_RANGE || yValue > MAX_COORDINATE_RANGE) {
+            throw new IllegalArgumentException("출력할 수 있는 좌표 범위를 벗어났습니다");
+        }
     }
 }
--- src/test/java/coordinatecalculator/view/OutputViewTest.java
+++ src/test/java/coordinatecalculator/view/OutputViewTest.java
@@ -1,11 +1,17 @@
 package coordinatecalculator.view;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatCode;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import coordinatecalculator.dto.PointDto;
+import coordinatecalculator.dto.PointsDto;
+import coordinatecalculator.view.printer.SpyPrinter;
 
 class OutputViewTest {
 
@@ -24,6 +30,66 @@ class OutputViewTest {
         @Test
         void createConsoleOutputViewTest() {
             assertThatCode(OutputView::createConsoleOutputView).doesNotThrowAnyException();
+        }
+    }
+
+    @DisplayName("좌표계 출력 테스트")
+    @Nested
+    class CoordinateTest {
+
+        @DisplayName("1 미만 또는 24 초과의 좌표를 입력하면 예외를 던진다")
+        @ParameterizedTest(name = "x좌표 : {0}, y좌표 : {1}")
+        @CsvSource({"0,12", "25,12", "12,0", "12,25"})
+        void printCoordinateTest_whenOverCoordinateRange_throwException(int x, int y) {
+            PointsDto pointsDto = PointsDto.of(PointDto.of(x, y));
+            OutputView outputView = OutputView.of(new SpyPrinter());
+
+            assertThatThrownBy(() -> outputView.printCoordinate(pointsDto))
+                    .isInstanceOf(IllegalArgumentException.class)
+                    .hasMessage("출력할 수 있는 좌표 범위를 벗어났습니다");
+        }
+
+        @DisplayName("1 미만 또는 24 초과의 좌표를 입력하면 예외를 던진다")
+        @Test
+        void printCoordinateTest() {
+            SpyPrinter printer = new SpyPrinter();
+            OutputView outputView = OutputView.of(printer);
+            PointsDto pointsDto = PointsDto.of(PointDto.of(10, 10), PointDto.of(14, 16));
+
+            outputView.printCoordinate(pointsDto);
+
+            assertThat(printer.getPrintedMessage()).isEqualTo(getCoordinate_10_10_and_14_16());
+        }
+
+        String getCoordinate_10_10_and_14_16() {
+            String lineSeparator = "\n";
+
+            return "24|                                                " + lineSeparator
+                    + "  |                                                " + lineSeparator
+                    + "22|                                                " + lineSeparator
+                    + "  |                                                " + lineSeparator
+                    + "20|                                                " + lineSeparator
+                    + "  |                                                " + lineSeparator
+                    + "18|                                                " + lineSeparator
+                    + "  |                                                " + lineSeparator
+                    + "16|                           •                    " + lineSeparator
+                    + "  |                                                " + lineSeparator
+                    + "14|                                                " + lineSeparator
+                    + "  |                                                " + lineSeparator
+                    + "12|                                                " + lineSeparator
+                    + "  |                                                " + lineSeparator
+                    + "10|                   •                            " + lineSeparator
+                    + "  |                                                " + lineSeparator
+                    + " 8|                                                " + lineSeparator
+                    + "  |                                                " + lineSeparator
+                    + " 6|                                                " + lineSeparator
+                    + "  |                                                " + lineSeparator
+                    + " 4|                                                " + lineSeparator
+                    + "  |                                                " + lineSeparator
+                    + " 2|                                                " + lineSeparator
+                    + "  |                                                " + lineSeparator
+                    + "  +ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ" + lineSeparator
+                    + " 0    2   4   6   8   10  12  14  16  18  20  22  24" + lineSeparator;
         }
     }
 }
