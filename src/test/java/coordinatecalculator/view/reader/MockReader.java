525492ba91e65a49d57e7d22e1fa31fff0905458
leegwichan <cnddkscndgus@naver.com>
2023-08-09 18:21:11 +0900

    test (MockReader) : 여러번 호출했을 때 다른 값을 제공할 수 있도록 구현

--- src/test/java/coordinatecalculator/view/reader/MockReader.java
+++ src/test/java/coordinatecalculator/view/reader/MockReader.java
@@ -1,21 +1,25 @@
 package coordinatecalculator.view.reader;
 
+import java.util.List;
+
 public class MockReader implements Reader {
 
-    private static final String DEFAULT_MESSAGE = "";
-
-    private final String message;
+    private final List<String> messages;
+    private int index = 0;
 
     public MockReader(String message) {
-        this.message = message;
+        this.messages = List.of(message);
     }
 
     public MockReader() {
-        this.message = DEFAULT_MESSAGE;
+        this.messages = List.of();
     }
 
     @Override
     public String read() {
-        return message;
+        if (index >= messages.size()) {
+            throw new IllegalStateException("더 이상 읽을 내용이 없습니다");
+        }
+        return messages.get(index++);
     }
 }
