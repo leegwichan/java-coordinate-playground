aa79b9288311806eb5788b5f48f6c0c657ad9524
DESKTOP-E5VRH4Q\cnddk <cnddkscndgus@naver.com>
2023-08-10 13:46:54 +0900

    feat (OutputView) : 좌표 출력 기능 구현

--- src/main/java/coordinatecalculator/view/OutputView.java
+++ src/main/java/coordinatecalculator/view/OutputView.java
@@ -1,6 +1,11 @@
 package coordinatecalculator.view;
 
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+import java.util.List;
 import java.util.Objects;
+import java.util.stream.IntStream;
 import coordinatecalculator.dto.PointDto;
 import coordinatecalculator.dto.PointsDto;
 import coordinatecalculator.view.printer.ConsolePrinter;
@@ -11,9 +16,15 @@ public final class OutputView {
     private static final int MAX_COORDINATE_RANGE = 24;
     private static final int MIN_COORDINATE_RANGE = 1;
 
+    private static final String ODD_PREFIX = "  |";
+    private static final String EVEN_PREFIX_FORMAT = "%2d|";
     private static final String BLANK = "  ";
-    private static final String POINT = " •";
+    private static final String DOT = " •";
     private static final String LINE_SEPARATOR = "\n";
+    private static final String X_AXIS
+            = "  +ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ" + LINE_SEPARATOR
+            + " 0    2   4   6   8   10  12  14  16  18  20  22  24" + LINE_SEPARATOR;
+
 
     private final Printer printer;
 
@@ -30,16 +41,21 @@ public final class OutputView {
     }
 
     public void printCoordinate(PointsDto points) {
-        validatePoints(points);
+        validate(points);
+        String result = IntStream.rangeClosed(MIN_COORDINATE_RANGE, MAX_COORDINATE_RANGE)
+                .map(yValue -> MIN_COORDINATE_RANGE + MAX_COORDINATE_RANGE - yValue)
+                .mapToObj(yValue -> getCoordinateLine(yValue, points))
+                .collect(joining()).concat(X_AXIS);
+        printer.print(result);
     }
 
-    private void validatePoints(PointsDto points) {
+    private void validate(PointsDto points) {
         for (PointDto point : points.getPoints()) {
-            validatePoint(point);
+            validate(point);
         }
     }
 
-    private void validatePoint(PointDto point) {
+    private void validate(PointDto point) {
         int xValue = point.getX();
         int yValue = point.getY();
 
@@ -47,5 +63,30 @@ public final class OutputView {
                 || yValue < MIN_COORDINATE_RANGE || yValue > MAX_COORDINATE_RANGE) {
             throw new IllegalArgumentException("출력할 수 있는 좌표 범위를 벗어났습니다");
         }
+    }
+
+    private String getCoordinateLine(int line, PointsDto points) {
+        List<Integer> xValues = points.getPoints().stream()
+                .filter(pointDto -> pointDto.getY() == line)
+                .map(PointDto::getX).collect(toList());
+
+        String coordinateResult = IntStream.rangeClosed(MIN_COORDINATE_RANGE, MAX_COORDINATE_RANGE)
+                .mapToObj(xValue -> getCoordinateResult(xValues.contains(xValue)))
+                .collect(joining());
+        return getCoordinateLinePrefix(line).concat(coordinateResult).concat(LINE_SEPARATOR);
+    }
+
+    private String getCoordinateLinePrefix(int line) {
+        if (line % 2 == 1) {
+            return ODD_PREFIX;
+        }
+        return String.format(EVEN_PREFIX_FORMAT, line);
+    }
+
+    private String getCoordinateResult(boolean isExist) {
+        if (isExist) {
+            return DOT;
+        }
+        return BLANK;
     }
 }
--- src/test/java/coordinatecalculator/view/OutputViewTest.java
+++ src/test/java/coordinatecalculator/view/OutputViewTest.java
@@ -37,6 +37,8 @@ class OutputViewTest {
     @Nested
     class CoordinateTest {
 
+        private static final String LINE_SEPARATOR = "\n";
+
         @DisplayName("1 미만 또는 24 초과의 좌표를 입력하면 예외를 던진다")
         @ParameterizedTest(name = "x좌표 : {0}, y좌표 : {1}")
         @CsvSource({"0,12", "25,12", "12,0", "12,25"})
@@ -49,9 +51,9 @@ class OutputViewTest {
                     .hasMessage("출력할 수 있는 좌표 범위를 벗어났습니다");
         }
 
-        @DisplayName("1 미만 또는 24 초과의 좌표를 입력하면 예외를 던진다")
+        @DisplayName("점들을 입력받으면 정상적으로 출력한다 (10,10), (14,16)")
         @Test
-        void printCoordinateTest() {
+        void printCoordinateTest_10_10_and_14_16() {
             SpyPrinter printer = new SpyPrinter();
             OutputView outputView = OutputView.of(printer);
             PointsDto pointsDto = PointsDto.of(PointDto.of(10, 10), PointDto.of(14, 16));
@@ -61,35 +63,74 @@ class OutputViewTest {
             assertThat(printer.getPrintedMessage()).isEqualTo(getCoordinate_10_10_and_14_16());
         }
 
-        String getCoordinate_10_10_and_14_16() {
-            String lineSeparator = "\n";
+        @DisplayName("점들을 입력받으면 정상적으로 출력한다 (5,5), (24,1)")
+        @Test
+        void printCoordinateTest_5_5_and_24_1() {
+            SpyPrinter printer = new SpyPrinter();
+            OutputView outputView = OutputView.of(printer);
+            PointsDto pointsDto = PointsDto.of(PointDto.of(5,5), PointDto.of(24,1));
 
-            return "24|                                                " + lineSeparator
-                    + "  |                                                " + lineSeparator
-                    + "22|                                                " + lineSeparator
-                    + "  |                                                " + lineSeparator
-                    + "20|                                                " + lineSeparator
-                    + "  |                                                " + lineSeparator
-                    + "18|                                                " + lineSeparator
-                    + "  |                                                " + lineSeparator
-                    + "16|                           •                    " + lineSeparator
-                    + "  |                                                " + lineSeparator
-                    + "14|                                                " + lineSeparator
-                    + "  |                                                " + lineSeparator
-                    + "12|                                                " + lineSeparator
-                    + "  |                                                " + lineSeparator
-                    + "10|                   •                            " + lineSeparator
-                    + "  |                                                " + lineSeparator
-                    + " 8|                                                " + lineSeparator
-                    + "  |                                                " + lineSeparator
-                    + " 6|                                                " + lineSeparator
-                    + "  |                                                " + lineSeparator
-                    + " 4|                                                " + lineSeparator
-                    + "  |                                                " + lineSeparator
-                    + " 2|                                                " + lineSeparator
-                    + "  |                                                " + lineSeparator
-                    + "  +ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ" + lineSeparator
-                    + " 0    2   4   6   8   10  12  14  16  18  20  22  24" + lineSeparator;
+            outputView.printCoordinate(pointsDto);
+
+            assertThat(printer.getPrintedMessage()).isEqualTo(getCoordinate_5_5_and_24_1());
+        }
+
+        String getCoordinate_10_10_and_14_16() {
+            return "24|                                                " + LINE_SEPARATOR
+                    + "  |                                                " + LINE_SEPARATOR
+                    + "22|                                                " + LINE_SEPARATOR
+                    + "  |                                                " + LINE_SEPARATOR
+                    + "20|                                                " + LINE_SEPARATOR
+                    + "  |                                                " + LINE_SEPARATOR
+                    + "18|                                                " + LINE_SEPARATOR
+                    + "  |                                                " + LINE_SEPARATOR
+                    + "16|                           •                    " + LINE_SEPARATOR
+                    + "  |                                                " + LINE_SEPARATOR
+                    + "14|                                                " + LINE_SEPARATOR
+                    + "  |                                                " + LINE_SEPARATOR
+                    + "12|                                                " + LINE_SEPARATOR
+                    + "  |                                                " + LINE_SEPARATOR
+                    + "10|                   •                            " + LINE_SEPARATOR
+                    + "  |                                                " + LINE_SEPARATOR
+                    + " 8|                                                " + LINE_SEPARATOR
+                    + "  |                                                " + LINE_SEPARATOR
+                    + " 6|                                                " + LINE_SEPARATOR
+                    + "  |                                                " + LINE_SEPARATOR
+                    + " 4|                                                " + LINE_SEPARATOR
+                    + "  |                                                " + LINE_SEPARATOR
+                    + " 2|                                                " + LINE_SEPARATOR
+                    + "  |                                                " + LINE_SEPARATOR
+                    + "  +ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ" + LINE_SEPARATOR
+                    + " 0    2   4   6   8   10  12  14  16  18  20  22  24" + LINE_SEPARATOR;
+        }
+
+        String getCoordinate_5_5_and_24_1() {
+            return "24|                                                " + LINE_SEPARATOR
+                    + "  |                                                " + LINE_SEPARATOR
+                    + "22|                                                " + LINE_SEPARATOR
+                    + "  |                                                " + LINE_SEPARATOR
+                    + "20|                                                " + LINE_SEPARATOR
+                    + "  |                                                " + LINE_SEPARATOR
+                    + "18|                                                " + LINE_SEPARATOR
+                    + "  |                                                " + LINE_SEPARATOR
+                    + "16|                                                " + LINE_SEPARATOR
+                    + "  |                                                " + LINE_SEPARATOR
+                    + "14|                                                " + LINE_SEPARATOR
+                    + "  |                                                " + LINE_SEPARATOR
+                    + "12|                                                " + LINE_SEPARATOR
+                    + "  |                                                " + LINE_SEPARATOR
+                    + "10|                                                " + LINE_SEPARATOR
+                    + "  |                                                " + LINE_SEPARATOR
+                    + " 8|                                                " + LINE_SEPARATOR
+                    + "  |                                                " + LINE_SEPARATOR
+                    + " 6|                                                " + LINE_SEPARATOR
+                    + "  |         •                                      " + LINE_SEPARATOR
+                    + " 4|                                                " + LINE_SEPARATOR
+                    + "  |                                                " + LINE_SEPARATOR
+                    + " 2|                                                " + LINE_SEPARATOR
+                    + "  |                                               •" + LINE_SEPARATOR
+                    + "  +ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ" + LINE_SEPARATOR
+                    + " 0    2   4   6   8   10  12  14  16  18  20  22  24" + LINE_SEPARATOR;
         }
     }
 }
